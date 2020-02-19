3661eb7ae6d442c221225f7270cbc70df4e64721
Gaurav Agarwal <32949625+gauravagarwal003@users.noreply.github.com>
2020-02-19 10:20:34 -0800

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+package examples.pltw.org;
+
+public class Arithmetic {
+    private int mOperand1;
+    private int mOperand2;
+
+    public Arithmetic() {
+        mOperand1 = 2;
+        mOperand2 = 3;
+    }
+
+    public String toString() {
+        return "Arithmetic Instance: mOperand1 = " + mOperand1 + "; mOperand 2 = " + mOperand2;
+    }
+}
